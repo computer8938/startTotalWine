c3e6518651b13845f1a2c84b464ef2c3daa2ff17
jhpark <computer8938@naver.com>
2022-01-09 04:19:20 +0900

    Dao 생성, 적용

---
+++ src/main/java/com/jhpark/tWine/dao/ArticleDao.java
@@ -0,0 +1,10 @@
+package com.jhpark.tWine.dao;
+
+import java.util.List;
+
+import com.jhpark.tWine.dto.ArticleDTO;
+
+public interface ArticleDao {
+	public List<ArticleDTO> getList();
+	
+}
--- src/main/java/com/jhpark/tWine/dao/ArticleDaoImpl.java
+++ src/main/java/com/jhpark/tWine/dao/ArticleDaoImpl.java
@@ -1,28 +1,21 @@
-package com.jhpark.tWine.service;
+package com.jhpark.tWine.dao;
 
-
+import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-import com.jhpark.tWine.dao.ArticleDao;
 import com.jhpark.tWine.dto.ArticleDTO;
 
-@Service
-public class ArticleServiceImpl implements ArticleService {
-	
-	@Autowired
-	ArticleDao articleDao;
-	// 2자적 본론으로, ArticleService에서 데이터 관련은 모두 Dao에서 처리하자
 
-	/*
+@Component //Autowired로 주입된 것들이 연결
+public class ArticleDaoImpl implements ArticleDao{
+
 	public List<ArticleDTO> getList(){
 		//1차적으로, DB에서 데이터를 가져오지 말고 가짜데이터를 뿌려본다.
 		ArticleDTO article1 = new ArticleDTO(1, "2022-01-09 03:12:40", "제목1", "내용1");
 		ArticleDTO article2 = new ArticleDTO(2, "2022-01-09 03:12:40", "제목2", "내용2");
 		ArticleDTO article3 = new ArticleDTO(3, "2022-01-09 03:12:40", "제목3", "내용3");
-		
 		
 		List<ArticleDTO> list = new ArrayList<>();
 		list.add(article1);
@@ -30,9 +23,5 @@ public class ArticleServiceImpl implements ArticleService {
 		list.add(article3);
 		
 		return list;
-	}
-	*/
-	public List<ArticleDTO> getList(){
-		return articleDao.getList();
 	}
 }
